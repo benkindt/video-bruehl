a4589ee156b1581cbf36050f11ca0db5bf521f19
benkindt <benjamin.kindt@s2011.tu-chemnitz.de>
2017-05-26 15:06:35 +0200

    text and fixes

--- js/player.js
+++ js/player.js
@@ -16,7 +16,141 @@ $(document).ready(function() {
         }]
     });
     
-    player.overlay({
+    function getXmlPresentation(){
+    	console.log("getting xml-presentation...");
+    	// XML to JSON with jquery and xml2json (https://github.com/sparkbuzz/jQuery-xml2json)    
+        $.ajax({
+            url: '../data/xml/interactive_media.xml',
+            dataType: 'xml',
+            success: function(response) {
+                json = $.xml2json(response);
+                console.log("success");
+                console.log(json);
+            },
+            error: function (request, status, error) {
+                console.log("failed");
+            	console.log(request);
+            }
+        });
+        console.log("json:");
+        console.log(json);
+    }
+    
+    getXmlPresentation();
+    
+    // bind interactive components
+    $('#getPresentation').on('click', function() {
+        getXmlPresentation();
+    });
+    
+    $('#getTime').on('click', function() {
+        console.log("current Time is: " + getTime());
+    });
+    
+    $('#showMetadata').on('click', function() {
+    	changeVideo($('#idInput').val());
+    });
+    
+});
+
+// function which handles all changes needed when a video is changed
+function changeVideo(id){
+	// gets the video-object for which data will be processed
+	var videoObject = getVideoObject(id);
+	console.log(videoObject);
+	// left box under the player
+	listMetadata(videoObject);
+	// middle box under the player
+	directionOutput(videoObject);
+	// right box under the player
+	showPois(videoObject);
+//	listPointsOfInterest(videoObject);
+	// change the video in the player
+	changeSource({ 
+        type: "video/mp4",
+        src: "../data/vid/" + videoObject['filename']
+    });
+}
+
+function initFirstVideo(){
+	changeVideo($('#idInput').val());
+	$("#init").css("display", "none");
+	$("#midWrapper").css("visibility", "visible");
+	$("#goBack").css("visibility","visible");
+}
+
+function getVideoObject(id){
+	for(var i = 0; i < json['#document']['interactive_media']['videos']['video'].length; i++) {
+		console.log("found next video object");
+		var obj = json['#document']['interactive_media']['videos']['video'][i];
+		if(obj.$.id == id){
+			return obj;
+		}
+	}
+}
+
+function resetVideo(){
+	changeVideo($('#idInput').val());
+}
+
+// function for the video neighbours
+function directionOutput(obj){
+	$("#left").css("visibility", "hidden");
+	$("#left").css("cursor", "auto");
+	$("#right").css("visibility", "hidden");
+	$("#right").css("cursor", "auto");
+	$("#turnaround").css("visibility", "hidden");
+	$("#turnaround").css("cursor", "auto");
+	$("#front").css("visibility", "hidden");
+	$("#front").css("cursor", "auto");
+	$("#frontleft").css("visibility", "hidden");
+	$("#frontleft").css("cursor", "auto");
+	// loop over the neighbours
+	for(var i = 0; i < obj["neighbours"]["neighbour"].length; i++) {
+	    var neigh = obj["neighbours"]["neighbour"][i];
+	    var direction = neigh.n_type;
+	    if(direction == "turnleft"){
+	    	$("#left").css("visibility", "visible");
+	    	$("#left").css("cursor", "pointer");
+	    	$("#left").attr("onclick", "changeVideo(" + neigh.n_id + ")");
+	    } else if (direction == "turnright") {
+	    	$("#right").css("visibility", "visible");
+	    	$("#right").css("cursor", "pointer");
+	    	$("#right").attr("onclick", "changeVideo(" + neigh.n_id + ")");
+	    } else if (direction == "turnaround") {
+	    	$("#turnaround").css("visibility", "visible");
+	    	$("#turnaround").css("cursor", "pointer");
+	    	$("#turnaround").attr("onclick", "changeVideo(" + neigh.n_id + ")");
+	    } else if (direction == "forward") {
+	    	$("#front").css("visibility", "visible");
+	    	$("#front").css("cursor", "pointer");
+	    	$("#front").attr("onclick", "changeVideo(" + neigh.n_id + ")");
+	    } else if (direction == "frontleft") {
+	    	$("#frontleft").css("visibility", "visible");
+	    	$("#frontleft").css("cursor", "pointer");
+	    	$("#frontleft").attr("onclick", "changeVideo(" + neigh.n_id + ")");
+	    }
+	}
+}
+
+function listMetadata(obj) {
+	$("#locationName").text(obj['street']);
+	$("#locationCoordinates").text(obj['coordinates']);
+	$("#locationDirection").text(obj['direction_of_view']);
+}
+
+function showPois(obj) {
+	console.log("shows poi");
+	console.log(obj);
+	// loop over the poi's
+	for(var i = 0; i < obj["pois"]["poi"].length; i++) {
+	    var poi = obj["pois"]["poi"][i];
+	    console.log(poi);
+	}
+}
+
+function createOverlay() {
+	player.overlay({
         content: '<div class="videoOverlay">&nbsp;&nbsp;Tante-Emma-Laden&nbsp;&nbsp;</div>',
         debug: true,
         showBackground: false,
@@ -41,146 +175,6 @@ $(document).ready(function() {
           end: 'pause'
         }]
       });
-    
-    function getXmlPresentation(){
-    	console.log("getting xml-presentation...");
-    	// XML to JSON with jquery and xml2json (https://github.com/sparkbuzz/jQuery-xml2json)    
-        $.ajax({
-            url: '../data/xml/interactive_media.xml',
-            dataType: 'xml',
-            success: function(response) {
-                json = $.xml2json(response);
-                console.log(json);
-            },
-            error: function (request, status, error) {
-                console.log(request);
-            }
-        });
-    }
-    
-    getXmlPresentation();
-    
-    // bind interactive components
-    $('#getPresentation').on('click', function() {
-        getXmlPresentation();
-    });
-    
-    $('#change1').on('click', function() {
-        changeSource({ 
-            type: "video/webm",
-            src: "http://upload.wikimedia.org/wikipedia/mediawiki/d/d1/Wikipedia_on_Google_Glass_screencast_test.webm"
-        });
-    });
-
-    $('#change2').on('click', function() {
-    	changeSource({ 
-            type: "video/mp4",
-            src: "../data/vid/SCF_0001.mp4"
-        });
-    });
-    
-    $('#getTime').on('click', function() {
-        console.log("current Time is: " + getTime());
-    });
-    
-    $('#showMetadata').on('click', function() {
-    	changeVideo($('#idInput').val());
-    });
-    
-});
-
-function changeVideo(id){
-	console.log(json['#document']['interactive_media']['videos']);
-	console.log(id);
-//	var videoObject = json['#document']['interactive_media']['videos']['video'][id];
-	var videoObject = getVideoObject(id);
-	console.log(videoObject);
-	directionOutput(videoObject);
-	listMetadata(videoObject);
-//	listPointsOfInterest(videoObject);
-	var sourceFile = "../data/vid/" + videoObject['filename'];
-	console.log(sourceFile);
-	changeSource({ 
-        type: "video/mp4",
-        src: sourceFile
-    });
-	showPois(videoObject);
-}
-
-function getVideoObject(id){
-	for(var i = 0; i < json['#document']['interactive_media']['videos']['video'].length; i++) {
-		console.log("found next video object");
-		var obj = json['#document']['interactive_media']['videos']['video'][i];
-		if(obj.$.id == id){
-			return obj;
-		}
-	}
-}
-
-function directionOutput(obj){
-	console.log("showing direction for " + obj);
-	var string = "";
-	$("#left").css("visibility", "hidden");
-	$("#left").css("cursor", "auto");
-	$("#right").css("visibility", "hidden");
-	$("#right").css("cursor", "auto");
-	$("#turnaround").css("visibility", "hidden");
-	$("#turnaround").css("cursor", "auto");
-	$("#front").css("visibility", "hidden");
-	$("#front").css("cursor", "auto");
-	$("#frontleft").css("visibility", "hidden");
-	$("#frontleft").css("cursor", "auto");
-	// loop over the neighbours
-	for(var i = 0; i < obj["neighbours"]["neighbour"].length; i++) {
-	    var neigh = obj["neighbours"]["neighbour"][i];
-	    console.log(neigh);
-	    var direction = neigh.n_type;
-	    var directionLabel = "";
-	    if(direction == "turnleft"){
-	    	directionLabel = "Nach links";
-	    	$("#left").css("visibility", "visible");
-	    	$("#left").css("cursor", "pointer");
-	    	$("#left").attr("onclick", "changeVideo(" + neigh.n_id + ")");
-	    } else if (direction == "turnright") {
-	    	directionLabel = "Nach rechts";
-	    	$("#right").css("visibility", "visible");
-	    	$("#right").css("cursor", "pointer");
-	    	$("#right").attr("onclick", "changeVideo(" + neigh.n_id + ")");
-	    } else if (direction == "turnaround") {
-	    	directionLabel = "Nach hinten";
-	    	$("#turnaround").css("visibility", "visible");
-	    	$("#turnaround").css("cursor", "pointer");
-	    	$("#turnaround").attr("onclick", "changeVideo(" + neigh.n_id + ")");
-	    } else if (direction == "forward") {
-	    	directionLabel = "Nach vorn";
-	    	$("#front").css("visibility", "visible");
-	    	$("#front").css("cursor", "pointer");
-	    	$("#front").attr("onclick", "changeVideo(" + neigh.n_id + ")");
-	    } else if (direction == "frontleft") {
-	    	directionLabel = "Nach links vorn"
-	    	$("#frontleft").css("visibility", "visible");
-	    	$("#frontleft").css("cursor", "pointer");
-	    	$("#frontleft").attr("onclick", "changeVideo(" + neigh.n_id + ")");
-	    }
-//	    string += "<span class='videoLink' onclick='changeVideo(" + neigh.n_id + ")'>" + directionLabel + "</span></br>"
-	}
-//	$("#directions").html(string);
-}
-
-function listMetadata(obj) {
-	$("#locationName").text(obj['street']);
-	$("#locationCoordinates").text(obj['coordinates']);
-	$("#locationDirection").text(obj['direction_of_view']);
-}
-
-function showPois(obj) {
-	console.log("show pois");
-	console.log(obj);
-	// loop over the poi's
-	for(var i = 0; i < obj["POIs"]["POI"].length; i++) {
-	    var poi = obj["POIs"]["POI"][i];
-	    console.log(poi);
-	}
 }
 	
 function changeSource(src) {
